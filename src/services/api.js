be6daf6886c7cc6f2a24df658626f550827847f1
Giovani Generali <giovani.generali@gmail.com>
2019-05-27 16:45:03 -0300

    update api baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import store from '../store';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_HOST_API || 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_HOST_API,
 });
 
 api.interceptors.request.use((config) => {
